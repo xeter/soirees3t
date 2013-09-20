ab80241c67b690bdacf0d4004d4e9ed7b47aafe2
faom <faom@mint>
2013-09-20 17:38:56 +0200

    Ajout du test qui permet de montrer comment faire pour ajouter une méthode à un DAO précis

--- springdata/codes_sources/sdjpabase/src/test/java/fr/soat/springdata/jpa/dao/personnalisees/supplementaire/PersonneDaoPersonnaliseeRepositoryTest.java
+++ springdata/codes_sources/sdjpabase/src/test/java/fr/soat/springdata/jpa/dao/personnalisees/supplementaire/PersonneDaoPersonnaliseeRepositoryTest.java
@@ -2,8 +2,7 @@ package fr.soat.springdata.jpa.dao.personnalisees.supplementaire;
 
 import java.util.List;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,31 +11,31 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 
-import fr.soat.springdata.jpa.dao.personnalisees.supplementaire.PersonneDaoPersonnaliseeRepository;
 import fr.soat.springdata.jpa.entites.Personne;
+import fr.soat.springdata.jpa.helpers.PersonneDataHelper;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @TransactionConfiguration(transactionManager="transactionManager")
 @ContextConfiguration("classpath:applicationContext.xml")
 public class PersonneDaoPersonnaliseeRepositoryTest {
-	
+
 	@Autowired
 	private PersonneDaoPersonnaliseeRepository personneDaoPersonnaliseeRepository;
 	private Personne personneSauvee;
 
 	@Before
 	public void setUp() {
-//		personneDaoPersonnaliseeRepository.deleteAll();
-//		this.personneSauvee = PersonneDataHelper.creePersonne();
-//		personneDaoPersonnaliseeRepository.save(this.personneSauvee);
+		this.personneDaoPersonnaliseeRepository.deleteAll();
+		this.personneSauvee = PersonneDataHelper.creePersonne();
+		this.personneDaoPersonnaliseeRepository.save(this.personneSauvee);
 	}
 
 	@Test
 	public void test() {
-//		List<Personne> personneList = 
-//				personneDaoPersonnaliseeRepository.uneMethodePersonnaliseeDeRechercheParNom(this.personneSauvee.getNom());
-//		Assert.assertEquals(1, personneList.size());
-//		Assert.assertEquals(this.personneSauvee.getNom(), personneList.get(0).getNom());
+		List<Personne> personneList =
+				this.personneDaoPersonnaliseeRepository.uneMethodePersonnaliseeDeRechercheParNom(this.personneSauvee.getNom());
+		Assert.assertEquals(1, personneList.size());
+		Assert.assertEquals(this.personneSauvee.getNom(), personneList.get(0).getNom());
 	}
 
 }
